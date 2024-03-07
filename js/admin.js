107ca9c1f409c81fda51983f5a4f0cbde5f032df
damanic <matt@mjman.net>
2024-03-07 22:55:39 +1100

    Escape select value (XSS protection)

--- js/admin.js
+++ js/admin.js
@@ -28,42 +28,44 @@ jQuery(document).ready(function($){
 
     $('.mjm-clinic-tagsadd').on("click", function(){
         var prefix =  $(this).data("mjm-clinic-ntdelbutton-prefix");
-        var dropdown = $('#'+prefix+'_selector');
+        var dropdown = $('#' + prefix + '_selector');
         var selected_name =  dropdown.children(':selected').text();
         var selected_id = dropdown.val();
-        var entry_element = $('#'+prefix+'_entry_'+selected_id);
-        var list_area =  $('#'+prefix+'_selections_area');
-        var selected_ids_storage_field =  $('#'+prefix+'_selected_ids');
+        var list_area =  $('#' + prefix + '_selections_area');
+        var selected_ids_storage_field =  $('#' + prefix + '_selected_ids');
 
-            //remove the element
-            if(!entry_element.length){
-                list_area.append(
-                    '<span id="'+prefix+'_entry_'+selected_id+'">' +
-                        '<a id="'+prefix+'_remove_'+selected_id+'"' +
-                        'class="ntdelbutton mjm-clinic-ntdelbutton"' +
-                        'data-mjm-clinic-ntdelbutton-prefix="'+prefix+'">X</a> ' +
-                        selected_name +
-                    '</span>'
-                );
-            }
+        // Create a span element and a remove button inside it
+        var entry_span = $('<span>').attr('id', prefix + '_entry_' + selected_id);
+        var remove_button = $('<a>').attr({
+            'id': prefix + '_remove_' + selected_id,
+            'class': 'ntdelbutton mjm-clinic-ntdelbutton',
+            'data-mjm-clinic-ntdelbutton-prefix': prefix,
+            'href': '#'
+        }).text('X');
 
-            //update the storage field
-            mjmClinicListField.add(selected_ids_storage_field,selected_id);
+        // Append the name as text to avoid XSS
+        entry_span.append(remove_button).append(document.createTextNode(' ' + selected_name));
+
+        // Ensure the element does not already exist to avoid duplicates
+        if($('#' + prefix + '_entry_' + selected_id).length === 0){
+            list_area.append(entry_span);
+        }
+
+        // Update the storage field
+        mjmClinicListField.add(selected_ids_storage_field, selected_id);
 
         return false;
-
     });
 
     $('.mjm-clinic-tagchecklist').on("click", '.mjm-clinic-ntdelbutton', function(){
         var el = $(this);
         var prefix = el.data("mjm-clinic-ntdelbutton-prefix");
-        var entry_id = el.attr('id').replace(prefix+'_remove_', '');
-        var entry_element = $('#'+prefix+'_entry_'+entry_id);
-        var selected_ids_storage_field =  $('#'+prefix+'_selected_ids');
+        var entry_id = el.attr('id').replace(prefix + '_remove_', '');
+        var entry_element = $('#' + prefix + '_entry_' + entry_id);
+        var selected_ids_storage_field = $('#' + prefix + '_selected_ids');
 
-
-        //remove the list entry
-        mjmClinicListField.remove(selected_ids_storage_field,entry_id);
+        // Remove the list entry
+        mjmClinicListField.remove(selected_ids_storage_field, entry_id);
         entry_element.remove();
 
         return false;
